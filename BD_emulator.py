5c71c0d52e0ae040fa84eed4c0fee410e67e8e0c
Михаил <ssosnovy1997@gmail.com>
2022-04-08 10:02:50 +0300

    v2

---
+++ BD_emulator.py
@@ -0,0 +1,123 @@
+
+def finding(regexp, colum):
+    return ("""[{
+		"_id": {
+			"$oid": "624d79d797054a2d9de0006c"
+		},
+		"id": "492059428",
+		"first_name": "\u041c\u0438\u0445\u0430\u0438\u043b",
+		"full_name": "\u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439 \u041c\u0438\u0445\u0430\u0438\u043b \u0421\u0435\u0440\u0433\u0435\u0435\u0432\u0438\u0447",
+		"email": "ssosnovy1997@gmail.com",
+		"phone_number": "+79175312197",
+		"address_city": "\u041c\u043e\u0441\u043a\u0432\u0430",
+		"address_street": "\u041b\u0438\u043f\u0435\u0446\u043a\u0430\u044f \u0443\u043b\u0438\u0446\u0430",
+		"address_house": "10\u043a1",
+		"address_entrance": "1",
+		"address_floor": "15",
+		"address_office": "101",
+		"location_latitude": "55.598467",
+		"location_longitude": "37.669917",
+		"amount_charged": "0",
+		"user_id": "35907324",
+		"user_agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.101 Mobile S",
+		"created_at": "2021-06-19T12:39:44.000Z",
+		"address_doorcode": "120"
+	},
+	{
+		"_id": {
+			"$oid": "624d7a75a7d319f9d0303f72"
+		},
+		"id": 670895458,
+		"first_name": "\u0410\u043d\u0442\u043e\u043d \u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439",
+		"full_name": "\u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439 \u0410\u043d\u0442\u043e\u043d",
+		"email": "hidradigital@gmail.com",
+		"phone_number": 79281556988,
+		"address_city": "\u0420\u043e\u0441\u0442\u043e\u0432-\u043d\u0430-\u0414\u043e\u043d\u0443",
+		"address_street": "\u0443\u043b\u0438\u0446\u0430 \u0417\u043e\u0440\u0433\u0435",
+		"address_house": 66,
+		"address_entrance": 4,
+		"address_floor": "",
+		"address_office": "",
+		"address_comment": "\u0417\u0430\u043a\u0430\u0437 \u043f\u0440\u0438\u043c\u0435\u0442 \u0420\u0435\u0433\u0438\u043d\u0430 (+7 928 103-88-26)",
+		"location_latitude": 47.229721,
+		"location_longitude": 39.630472,
+		"amount_charged": 1514,
+		"user_id": 4533441,
+		"user_agent": "ios(5.25.0)",
+		"created_at": "2021-11-28T17:16:15.000Z",
+		"address_doorcode": ""
+	},
+	{
+		"_id": {
+			"$oid": "624d7a75a7d319f9d0303f73"
+		},
+		"id": 499151007,
+		"first_name": "\u0410\u043d\u0442\u043e\u043d \u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439",
+		"full_name": "\u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439 \u0410\u043d\u0442\u043e\u043d",
+		"email": "hidradigital@gmail.com",
+		"phone_number": 79281556988,
+		"address_city": "\u0420\u043e\u0441\u0442\u043e\u0432-\u043d\u0430-\u0414\u043e\u043d\u0443",
+		"address_street": "\u0431\u0443\u043b\u044c\u0432\u0430\u0440 \u041a\u043e\u043c\u0430\u0440\u043e\u0432\u0430",
+		"address_house": "1\u0418\u04412",
+		"address_entrance": 1,
+		"address_floor": 18,
+		"address_office": 180,
+		"address_comment": "\u0441\u0440\u0435\u0434\u043d\u0438\u0439 \u0434\u043e\u043c",
+		"location_latitude": 47.289941,
+		"location_longitude": 39.687704,
+		"amount_charged": 3199,
+		"user_id": 4533441,
+		"user_agent": "ios(5.15.0)",
+		"created_at": "2021-06-25T14:00:37.000Z",
+		"address_doorcode": "\u041a\u043e\u043d\u0441\u044c\u0435\u0440\u0436"
+	},
+	{
+		"_id": {
+			"$oid": "624d7b4fcf16c544492cdd24"
+		},
+		"id": 708553357,
+		"first_name": "\u041c\u0438\u0445\u0430\u0438\u043b",
+		"full_name": "\u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439 \u041c\u0438\u0445\u0430\u0438\u043b",
+		"email": "sosnoviymihail@yandex.ru",
+		"phone_number": 79189808488,
+		"address_city": "\u041a\u0440\u0430\u0441\u043d\u043e\u0434\u0430\u0440",
+		"address_street": "\u043f\u0440\u043e\u0435\u0437\u0434 \u0420\u0435\u043f\u0438\u043d\u0430",
+		"address_house": 40,
+		"address_entrance": 2,
+		"address_floor": 14,
+		"address_office": 119,
+		"address_comment": "",
+		"location_latitude": 45.07296,
+		"location_longitude": 38.977692,
+		"amount_charged": 508,
+		"user_id": 81028510,
+		"user_agent": "Mozilla/5.0 (Linux; Android 11; SM-G780G Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobi Superapp/Eats EatsKit/3.5.0",
+		"created_at": "2022-01-09T19:42:48.000Z",
+		"address_doorcode": "119\u0432"
+	},
+	{
+		"_id": {
+			"$oid": "624d7beb9c545083f3e6990e"
+		},
+		"id": 670895458,
+		"first_name": "\u0410\u043d\u0442\u043e\u043d \u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439",
+		"full_name": "\u0421\u043e\u0441\u043d\u043e\u0432\u044b\u0439 \u0410\u043d\u0442\u043e\u043d",
+		"email": "hidradigital@gmail.com",
+		"phone_number": 79281556988,
+		"address_city": "\u0420\u043e\u0441\u0442\u043e\u0432-\u043d\u0430-\u0414\u043e\u043d\u0443",
+		"address_street": "\u0443\u043b\u0438\u0446\u0430 \u0417\u043e\u0440\u0433\u0435",
+		"address_house": 66,
+		"address_entrance": 4,
+		"address_floor": "",
+		"address_office": "",
+		"address_comment": "\u0417\u0430\u043a\u0430\u0437 \u043f\u0440\u0438\u043c\u0435\u0442 \u0420\u0435\u0433\u0438\u043d\u0430 (+7 928 103-88-26)",
+		"location_latitude": 47.229721,
+		"location_longitude": 39.630472,
+		"amount_charged": 1514,
+		"user_id": 4533441,
+		"user_agent": "ios(5.25.0)",
+		"created_at": "2021-11-28T17:16:15.000Z",
+		"address_doorcode": ""
+	}
+]
+""")
